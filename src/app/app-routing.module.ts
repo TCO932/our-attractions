41e6d467c551eb775f7dd48a3b424650f6c57e85
TCO932 <64690879+TCO932@users.noreply.github.com>
2022-11-19 19:48:14 +0400

    основа

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AttractionComponent } from './pages/attraction/attraction.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MainComponent } from './pages/main/main.component';
+import { NewAttractionComponent } from './pages/new-attraction/new-attraction.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { SearchComponent } from './pages/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'attraction', component: AttractionComponent },
+  { path: 'new-attraction', component: NewAttractionComponent },
+  { path: 'search', component: SearchComponent },
+  { path: '**',   redirectTo: 'main', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+
+import { MainComponent } from './pages/main/main.component';
+import { SearchComponent } from './pages/search/search.component';
+import { AttractionComponent } from './pages/attraction/attraction.component';
+import { NewAttractionComponent } from './pages/new-attraction/new-attraction.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SearchComponent,
+    AttractionComponent,
+    NewAttractionComponent,
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule,
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
